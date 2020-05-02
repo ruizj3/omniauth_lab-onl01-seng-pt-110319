898200d40ddbae934a262a2e48d6dac0e4b204ed
Joseph Ruiz <ruizj3@uw.edu>
2020-05-02 20:16:20 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Add your routes here
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-
+  get '/' => "welcome#index"
   root :to => "welcome#index"
 end
